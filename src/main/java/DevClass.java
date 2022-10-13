eecb258382e19b57e7e9262a8dfd1b0e2d306a0d
Ajay Wani <ajay.wani1986@gmail.com>
2022-10-13 17:20:24 +0530

    committed by Ajay after 2nd time change

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -2,5 +2,6 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("Dev Class");
         //Ajay changed this code
+        //Ajay changed second time
     }
 }
